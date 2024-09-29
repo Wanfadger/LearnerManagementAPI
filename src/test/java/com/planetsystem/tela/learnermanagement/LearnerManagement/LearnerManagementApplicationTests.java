8561d7192f41d40680944054442efd03bbdd0141
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-29 18:14:55 +0300

    added LearnerManagementApplicationTests

---
+++ src/test/java/com/planetsystem/tela/learnermanagement/LearnerManagement/LearnerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetsystem.tela.learnermanagement.LearnerManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
