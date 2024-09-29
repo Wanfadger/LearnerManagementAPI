91c6a5302e4df00f19d0567349e8d5910f3451f8
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-29 18:19:58 +0300

    added LearnerManagementApplication

---
+++ src/main/java/com/planetsystem/tela/learnermanagement/LearnerManagement/LearnerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.planetsystem.tela.learnermanagement.LearnerManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerManagementApplication.class, args);
+	}
+
+}
